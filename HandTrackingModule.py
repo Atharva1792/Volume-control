6054bd10f3389cc05cf0ab7be52b6abd649035c0
Atharva1792 <atharvaoverlord02@gmail.com>
2024-02-26 12:28:45 +0530

    Update

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 import math
-
+import pandas
 class handDetector():
     
     def __init__(self,mode=False,maxHands=2, detectionCon=0.5,trackCon=0.5):
