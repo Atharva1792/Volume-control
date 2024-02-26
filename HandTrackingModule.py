b364db2aedb9739119e8a2a22a2faf64fbd97661
Atharva1792 <atharvaoverlord02@gmail.com>
2024-02-26 16:11:08 +0530

    Update

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,7 +2,6 @@ import cv2
 import mediapipe as mp
 import time
 import math
-import pandas
 class handDetector():
     
     def __init__(self,mode=False,maxHands=2, detectionCon=0.5,trackCon=0.5):
@@ -11,11 +10,11 @@ class handDetector():
         self.detectionCon = detectionCon
         self.trackCon = trackCon
         
-        self.mphands = mp.solutions.hands
-        self.hands = self.mphands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackCon)
-        
+        '''self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionCon,self.trackCon)
+            
         self.mpDraw = mp.solutions.drawing_utils
-        self.tipIds = [4,8,12,16,20]
+        self.tipIds = [4,8,12,16,20]'''
         
     def findHands(self, img,draw=True):
         imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
